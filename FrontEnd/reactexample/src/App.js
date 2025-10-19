0c9c70ac01602b7ebe24483acddd997cd710e2ea
venkat sai <bvskrishna1219@gmail.com>
2025-10-19 09:21:12 +0530

    commiting p

---
+++ FrontEnd/reactexample/src/App.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+import './App.css';
+import DemoComponent from './component/DemoComponent';
+// import TestComponent from './component/TestComponent';
+// import TestComponent from './component/TestComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <DemoComponent />
+      {/* <TestComponent/> */}
+      {/* <input placeholder='Enter'/> */}
+      </div>
+  );
+} 
+export default App;
---
+++ FrontEnd/reactexample/src/component/DemoComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TestComponent from './TestComponent'
+
+const DemoComponent = () => {
+  return (
+    <div>DemoComponent
+      <TestComponent/>
+    </div>
+  )
+} 
+
+export default DemoComponent
